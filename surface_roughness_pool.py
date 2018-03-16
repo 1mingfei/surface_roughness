ed65f6ad001276317574bd030b8bc09c0b92feea
1mingfei <mingfei@umich.edu>
2018-03-16 12:16:53 -0400

    change python multiprocessing code to async

--- surface_roughness_pool.py
+++ surface_roughness_pool.py
@@ -6,14 +6,14 @@ from numpy import fft
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def get_surf_atoms(limits):
-    aa=fx.info('example/dump.000001677.cfg','cfg',2)
+def get_surf_atoms(cell,data,n_type,limits):
+    #aa=fx.info('example/dump.000001677.cfg','cfg',2)
     #aa=fx.info(a,'cfg',tot_num)
-    cell=aa.cell
-    data=aa.data     
-    n_type=aa.atom_type_num
+    #cell=aa.cell
+    #data=aa.data     
+    #n_type=aa.atom_type_num
     tot_num=int(len(data))
-    surface_atoms=[]
+    #surface_atoms=[]
     l2 = [data[i] for i in range(tot_num) if data[i][1] > limits[0] and  data[i][1] < limits[1] and  data[i][2] > limits[2] and  data[i][2] < limits[3] and  data[i][0]==1.07868200e+02 ]
     l2 = sorted(l2,key=lambda x:x[3],reverse=True)
     if len(l2)==0: pass
@@ -38,7 +38,7 @@ def surface_atoms_filter(a,N_type,N_space):
     xx, yy = np.meshgrid(np.linspace(0.0, 1.0, N_space-1),np.linspace(0.0, 1.0, N_space-1))
     M = np.array([[ x1, x1+1.0/float(N_space), x2, x2+1.0/float(N_space)] for x1, x2 in zip(np.ravel(xx), np.ravel(yy))])
     pool = multiprocessing.Pool(processes=8)
-    surface_atoms = pool.map(get_surf_atoms,M)
+    surface_atoms = [pool.apply_async(get_surf_atoms, args=(cell,data,n_type,M[i])).get() for i in range(len(M))]
     surface_atoms=[x for x in surface_atoms if x is not None]
     surface_atoms=np.asarray(surface_atoms)
 
@@ -48,8 +48,8 @@ def surface_atoms_filter(a,N_type,N_space):
         for i in range(len(surface_atoms)):
             fout.write("%12.8f   %12.8f   %12.8f   %12.8f\n"%(surface_atoms[i][0],surface_atoms[i][1],surface_atoms[i][2],surface_atoms[i][3]))
 
-    #print np.asarray(surface_atoms) 
-    #print(len(surface_atoms))
+    print np.asarray(surface_atoms) 
+    print(len(surface_atoms))
     aa.data=np.asarray(surface_atoms)
     aa.tot_num=int(len(aa.data))
     aa.atom_type_num=[n_type[1]]
@@ -119,5 +119,5 @@ def surface_statistics(a,N_space):
     return
 
 N=50
-#surface_atoms_filter('example/dump.000001677.cfg',2,N)
+surface_atoms_filter('example/dump.000001677.cfg',2,N)
 surface_statistics('surf.dat',N)
